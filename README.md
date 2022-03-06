cba6d4c8793256b43281d368c25928a6facdebd2
Karl Palsson <karlp@tweak.net.au>
2022-03-06 23:34:45 +0000

    Add notes on exploring with an older jlink

--- README.md
+++ README.md
@@ -26,6 +26,12 @@ Using J-link with serial: 000440182579
 
 'ts' is the time in milliseconds since the jlink was powered on.
 
+### Requirements
+This works with the jlink on the "WSTK" BRD4001A. (EFR32 boards, jlink has ethernet, quite recent)
+It does **not** work with the jlink on BRD2012A EFM32 Happy Gecko.  However, I've got some packet traces,
+and have partially decoded protocol there.  It's not really a target for me at this point though
+so this may go no further.
+
 ### Building
 Needs *libjaylink* and a C compiler.
 libjaylink-devel is available for fedora at least, if not, get it from [libjaylink](https://gitlab.zapb.de/libjaylink/libjaylink)
@@ -33,3 +39,55 @@ libjaylink-devel is available for fedora at least, if not, get it from [libjayli
 ## wireshark usb decoder for jlink traffic
 
 it's not _remotely_ finished, but the stub is there to plug in a statemachine....
+
+### useful bits of decoder help...
+```
+x = "abcdefg...some-hex-string...." (right click from wireshark copy as hex stream)
+>>> struct.unpack("<Ifff", binascii.unhexlify(x[32:64]))
+(1079682453, 3.416356325149536, 3.4178173542022705, 3.420008897781372)
+
+```
+
+### ssv5-aem-polling-old-efm32hg.pcapng
+This is a trace to try and make aem-dump work on an older EFM32HG board.
+from SSv5, this is "the same" but clearly quite different internally.
+
+This is just a blinking led, toggling between 3.4mA and 3.9mA, at 100ms in each state
+frame 18168 (1024 byte chunk) appears to be a continous chunk of floats of current?
+however, it's 256 readings, at what interval?! we _only_ see 3.4mA in it?
+18166, just before, first line could be 1000, 256, 1462?  (10khz, 256 records in next sample?)
+time is 9.291752, next 1k chunk comes at time 9.305747. 
+
+frame 18312 at 9.367962 (delta 70ms) contains 256 floats of ~3.91, so this is definitely 
+current measurements, yay.
+
+
+from the top, we see...
+emucom read 0x10002, 4096.
+< 4bytes: 060400
+< 32bytes: stuff, always eaeaaeae....... lots of zeros
+< 64bytes: ? voltage readings perhaps? (at least offset 0x60 seems to be voltage as a float?)
+< 1024b bytes: these are 256 le floats, with current in mA...
+then it repeats...
+but, if first reply is a 0000, then there' "nothing else?"
+and we move to check the SWO channel?
+
+there's also emucom ch 0x1000b in action?
+
+
+Ok, so summary:
+emucom 1000b is swo, it reads the channel, sees no data, moves on
+emucom 10002 is aem, it reads the channel, gets four replies.
+packet 4 has currents, just packed LE floats, packet 3 has... a voltage in it?
+packet 3 also starts with 10000, which might be a sampling rate?
+packet 1 and 2 are presumably metadata and timing information?
+
+### ssv5-aem-polling-old-efm32hg-with-start.pcapng
+As above, but recorded from before SSv5 turned on the energy profiler.
+I _believe_ the only interesting packet is 93/625, where it does a write to emucom ch 0x10002
+I believe this is turning on AEM, and then reading back to confirm in frames 100/632.
+I believe the duplication is probably just generally shitty softwware reading things in multiple places?
+After that, we just see the "normal" four chunk transfers starting at frame 3372
+
+### The others
+(I don't have exact details of them anymore, but current in the microamps, and aem-dump worked on them...)
--- wireshark_jlink_stub_plugin.lua
+++ wireshark_jlink_stub_plugin.lua
@@ -9,15 +9,36 @@
 p_jlink = Proto("jlink", "jlink USB Protocol")
 
 -- known jlink commands.  Sourced from "RM08001-R2 Reference manual for J-Link USB Protocol 2008"
+-- expanded from libjaylink
 local command_types = {
 	-- TODO - make better FSM wrapping, this is just a "how many in's are expected..."
-    [0x1] = "EMU_CMD_VERSION",
+    [0x00] = "EMU_CMD_SERVER",
+    [0x01] = "EMU_CMD_GET_VERSION",
+    [0x05] = "EMU_CMD_SET_SPEED",
+    [0x07] = "EMU_CMD_CLIENT",
+    [0x07] = "EMU_CMD_GET_HW_STATUS",
+    [0x08] = "EMU_CMD_SET_TARGET_POWER",
+    [0x09] = "EMU_CMD_REGISTER",
+    [0x1e] = "EMU_CMD_FILE_IO",
     [0xc0] = "EMU_CMD_GET_SPEEDS",
-    [0xd4] = "EMU_CMD_GET_MAX_MEM_BLOCK",
+    [0xc1] = "EMU_CMD_GET_HW_INFO",
+    [0xc2] = "EMU_CMD_GET_COUNTERS",
+    [0xc7] = "EMU_CMD_SELECT_TIF",
+    [0xce] = "EMU_CMD_JTAG_IO_V2",
+    [0xcf] = "EMU_CMD_JTAG_IO_V3",
+    [0xcf] = "EMU_CMD_SWD_IO",
+    [0xd4] = "EMU_CMD_GET_FREE_MEMORY",
+    [0xdc] = "EMU_CMD_CLEAR_RESET",
+    [0xdd] = "EMU_CMD_SET_RESET",
+    [0xde] = "EMU_CMD_JTAG_CLEAR_TRST",
+    [0xdf] = "EMU_CMD_JTAG_SET_TRST",
     [0xe8] = "EMU_CMD_GET_CAPS",
+    [0xeb] = "EMU_CMD_SWO",
     [0xed] = "EMU_CMD_GET_EXT_CAPS",
     [0xee] = "EMU_CMD_EMUCOM",
     [0xf0] = "EMU_CMD_GET_HW_VERSION",
+    [0xf2] = "EMU_CMD_READ_CONFIG",
+    [0xf3] = "EMU_CMD_WRITE_CONFIG",
 }
 
 local emucom_cmds = {
@@ -26,9 +47,9 @@ local emucom_cmds = {
 }
 
 p_jlink.fields.command = ProtoField.uint8("jlink.command", "Command Type", base.HEX, command_types)
-p_jlink.fields.emucom_cmd = ProtoField.uint8("jlink.emucom_cmd", "EMUCOM Command", base.DEC, emucom_cmds)
-p_jlink.fields.emucom_chan = ProtoField.uint32("jlink.emucom_chan", "EMUCOM Channel", base.HEX)
-p_jlink.fields.emucom_len = ProtoField.uint32("jlink.emucom_len", "EMUCOM Length", base.DEC)
+p_jlink.fields.emucom_cmd = ProtoField.uint8("jlink.emucom.cmd", "EMUCOM Command", base.DEC, emucom_cmds)
+p_jlink.fields.emucom_chan = ProtoField.uint32("jlink.emucom.chan", "EMUCOM Channel", base.HEX)
+p_jlink.fields.emucom_len = ProtoField.uint32("jlink.emucom.len", "EMUCOM Length", base.DEC)
 p_jlink.fields.unknown  = ProtoField.bytes("jlink.unknown", "Unidentified message data")
 
 -- Referenced USB URB dissector fields.
@@ -37,11 +58,10 @@ local f_transfer_type = Field.new("usb.transfer_type")
 local f_endpoint = Field.new("usb.endpoint_address.number")
 local f_endpoint_dir = Field.new("usb.endpoint_address.direction")
 
--- Insert warning for undecoded leftover data.
-local function warn_undecoded(tree, range)
-    local item = tree:add(p_jlink.fields.unknown, range)
-    item:add_expert_info(PI_UNDECODED, PI_WARN, "Leftover data")
-end
+-- Things we want to reference...
+local f_e_cmd = Field.new("jlink.emucom.cmd")
+local f_e_chan = Field.new("jlink.emucom.chan")
+local f_e_len = Field.new("jlink.emucom.len")
 
 function p_jlink.dissector(tvb, pinfo, tree)
 
@@ -50,30 +70,30 @@ function p_jlink.dissector(tvb, pinfo, tree)
 	local ep_dir = f_endpoint_dir().value -- 0 == out, 1 =0 in
 	--print("ep, epdir, with types", ep, tostring(type(ep)), ep_dir, tostring(type(ep_dir)))
 	assert(transfer_type==3) -- we're onyl adding ourselves to the bulk table right now.... duh
-	assert(ep == 3)
+	--assert(ep == 3) - no, depends very much on config, msc, msc+cdc, don't try this.
 
 	pinfo.cols.protocol = p_jlink.name
-	local subtree = tree:add(p_jlink, tvb(), "Jlink")
+	local subtree = tree:add(p_jlink, tvb(), "JLink")
 
-        local info_s = "unknown"
+    local info_s = "unknown"
 
 	if ep_dir == 0 then
 		local cmd = tvb(0,1):uint()
-		--print("cmd = ", cmd, type(cmd))
-		subtree:add(p_jlink.fields.command, tvb(0,1)):set_generated()
+		subtree:add(p_jlink.fields.command, tvb(0,1))
 		local mode = command_types[cmd]
-		info_s = mode
+		info_s = mode and mode or string.format("unknown out: %d(0x%02x)", cmd, cmd)
 		-- set expected to mode[1]....
                 if mode == "EMU_CMD_EMUCOM" then
-                    info_s = string.format("%s %s", info_s, emucom_cmds[tvb(1,1):uint()])
-                    subtree:add(p_jlink.fields.emucom_cmd, tvb(1,1)):set_generated()
-                    subtree:add_le(p_jlink.fields.emucom_chan, tvb(2,4)):set_generated()
-                    subtree:add_le(p_jlink.fields.emucom_len, tvb(6,4)):set_generated()
+                    subtree:add(p_jlink.fields.emucom_cmd, tvb(1,1))
+                    subtree:add_le(p_jlink.fields.emucom_chan, tvb(2,4))
+                    subtree:add_le(p_jlink.fields.emucom_len, tvb(6,4))
+                    info_s = string.format("%s %s ch: %x len: %d", mode, f_e_cmd().display, f_e_chan().value, f_e_len().value)
                 end
 	else
 		-- directin == in.  should be in the other mode now.... bump the state machine :)
+        info_s = "unknown in"
 	end
-        pinfo.cols.info:set(info_s)
+    pinfo.cols.info:set(info_s)
 
 
 
